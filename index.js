484c8a5a963c95f81839b51e9e5372a6e88e9ba8
yossi katzman <issoyk@gmail.com>
2018-02-14 19:38:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,21 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var lowercase = "hello"
   var uppercase ="HELLO"
+<<<<<<< HEAD
   var love = "I love you, Grandma."
+=======
+  var love =
+>>>>>>> 8437c11db6bcd2a2485ecbcf0226166ef962d97c
   if (string === lowercase) {
     return "I can't hear you!"
   }
    else if (string === uppercase) {
      return "YES INDEED!"
+<<<<<<< HEAD
    } else if (string === love) {
      return "I love you, too."
+=======
+>>>>>>> 8437c11db6bcd2a2485ecbcf0226166ef962d97c
    }
   
   
